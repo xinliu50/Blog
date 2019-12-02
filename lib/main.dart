2a13d986d44ca54fe180a7c574dd9d48aa12a95b
Xin Liu <xinliu253@yahoo.com>
2019-12-01 23:21:58 -0500

    Create register pagge

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blop_app/LoginRegisterPage.dart';
+import 'LoginRegisterPage.dart';
+
+void main(){
+  runApp(new BlogApp());
+}
+
+class BlogApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Blog App",
+      theme: new ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: LoginRegisterPage(),
+
+    );
+  }
+}
